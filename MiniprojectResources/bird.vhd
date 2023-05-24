0e245442c8d74f1e9b79c0e7bb387e1a1f5fc8ad
avel390 <avel390@aucklanduni.ac.nz>
2023-05-24 13:41:26 +1200

    Created initial Bird Movement

--- MiniprojectResources/bird.vhd
+++ MiniprojectResources/bird.vhd
@@ -6,8 +6,44 @@ USE  IEEE.STD_LOGIC_SIGNED.all;
 
 ENTITY bird IS
 	PORT
-		( pb1, pb2, clk, vert_sync	: IN std_logic;
-          pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
-		  red, green, blue 			: OUT std_logic);		
+		( clk, vert_sync, left_button, reset	: IN std_logic;
+          pixel_row, pixel_column				: IN std_logic_vector(9 DOWNTO 0);
+		  red, green, blue 						: OUT std_logic);		
 END bird;
 
+architecture behavior of bird is
+SIGNAL bird_on					: std_logic;
+SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
+SIGNAL bird_y_pos				: std_logic_vector(9 DOWNTO 0);
+SIGNAL bird_x_pos				: std_logic_vector(10 DOWNTO 0);
+SIGNAL bird_y_motion			: std_logic_vector(9 DOWNTO 0);
+
+BEGIN
+
+size <= CONV_STD_LOGIC_VECTOR(8,10);
+
+bird_x_pos <= CONV_STD_LOGIC_VECTOR(200,11);
+
+
+Red <= '1';
+
+Green <= not bird_on;
+Blue <=  not bird_on;
+
+Move_Bird: process (vert_sync, left_button, reset)
+begin
+	if (rising_edge(vert_sync)) then
+		if ( ('0' & ball_y_pos >= CONV_STD_LOGIC_VECTOR(479,10) - size) ) then
+			bird_on <= '0';
+		elsif (ball_y_pos <= size) then
+			bird_on <= '0';
+		elsif (left_button <= '1') then
+			bird_y_motion <= - CONV_STD_LOGIC_VECTOR(2,10);
+		else
+			bird_y_motion <= CONV_STD_LOGIC_VECTOR(2,10);
+		end if
+		bird_y_pos <= bird_y_pos + bird_y_motion;
+	end if
+end process Move_Bird;
+
+END behavior;
