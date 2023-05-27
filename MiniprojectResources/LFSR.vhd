067abf40c1caa7b612bd0b497cfdd0d5886de8cf
avel390 <avel390@aucklanduni.ac.nz>
2023-05-27 13:19:56 +1200

    Implemented behaviour of LFSR

--- MiniprojectResources/LFSR.vhd
+++ MiniprojectResources/LFSR.vhd
@@ -6,9 +6,33 @@ USE  IEEE.STD_LOGIC_SIGNED.all;
 
 entity LFSR is
 	port ( clk, reset : IN std_logic;
-		   pipe :	OUT std_logic );
+		   LSFR_value :	OUT std_logic_vector(4 downto 0));
 		   
 end LSFR;
 
+architecture behaviour of LFSR is
+SIGNAL seed: std_logic_vector(4 downto 0) := "10000";
+SIGNAL current_value: std_logic_vector(4 downto 0);
+SIGNAL next_value: std_logic_vector(4 downto 0);
+SIGNAL feedback : std_logic;
+begin
+
+value_gen : process (clk, reset)
+begin
+	if (rising_edge(clk)) then
+		if (reset = '1') then
+			current_value <= seed;
+		else
+			current_value <= next_value;
+		end if;
+	end if;
+end process;
+
+feedback <= current_value(3) xor current_value(2);
+next_value <= feedback & current_value(4 downto 1);
+
+LSFR_value <= current_value;
+
+END behaviour;
 
 	
