0c2480205cf820120ccdf31062eba717aae3e656
avel390 <avel390@aucklanduni.ac.nz>
2023-05-27 12:35:56 +1200

    Created initial LFSR file

---
+++ MiniprojectResources/LFSR.vhd
@@ -0,0 +1,14 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE  IEEE.STD_LOGIC_ARITH.all;
+USE  IEEE.STD_LOGIC_SIGNED.all;
+
+
+entity LFSR is
+	port ( clk, reset : IN std_logic;
+		   pipe :	OUT std_logic );
+		   
+end LSFR;
+
+
+	
